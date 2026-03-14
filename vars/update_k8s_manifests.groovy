1eb4f8ca7543eee1273c51bff41a6d322ce6f412
sauravmishra07 <157794482+sauravmishra07@users.noreply.github.com>
2026-03-14 18:37:52 +0530

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -25,17 +25,12 @@ def call(Map config = [:]) {
         
         // Update deployment manifests with new image tags - using proper Linux sed syntax
         sh """
-            # Update main application deployment - note the correct image name is trainwithshubham/easyshop-app
-            sed -i "s|image: trainwithshubham/easyshop-app:.*|image: trainwithshubham/easyshop-app:${imageTag}|g" ${manifestsPath}/08-easyshop-deployment.yaml
-            
-            # Update migration job if it exists
-            if [ -f "${manifestsPath}/12-migration-job.yaml" ]; then
-                sed -i "s|image: trainwithshubham/easyshop-migration:.*|image: trainwithshubham/easyshop-migration:${imageTag}|g" ${manifestsPath}/12-migration-job.yaml
-            fi
+            # Update main application deployment - note the correct image name is sauravmishra07/jobportal
+            sed -i "s|image: sauravmishra07/jobportal:.*|image: sauravmishra/jobportal:${imageTag}|g" ${manifestsPath}/07-jobportal-deployment.yaml
             
             # Ensure ingress is using the correct domain
-            if [ -f "${manifestsPath}/10-ingress.yaml" ]; then
-                sed -i "s|host: .*|host: easyshop.letsdeployit.com|g" ${manifestsPath}/10-ingress.yaml
+            if [ -f "${manifestsPath}/09-ingress.yaml" ]; then
+                sed -i "s|host: .*|host: jobportal.nip.io|g" ${manifestsPath}/09-ingress.yaml
             fi
             
             # Check for changes
@@ -47,7 +42,7 @@ def call(Map config = [:]) {
                 git commit -m "Update image tags to ${imageTag} and ensure correct domain [ci skip]"
                 
                 # Set up credentials for push
-                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/LondheShubham153/tws-e-commerce-app.git
+                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/sauravmishra07/Jobportal-DevOps-Project.git
                 git push origin HEAD:\${GIT_BRANCH}
             fi
         """
